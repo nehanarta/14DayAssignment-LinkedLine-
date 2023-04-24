8462e3d2815467bc8e0b5dc5319595ae3354defc
nehanarta <nehabaswanthrao@gmail.com>
2023-04-24 12:29:43 +0530

    Added:searchelment

--- LinkedList.java
+++ LinkedList.java
@@ -1,4 +1,4 @@
-public class LinkedList<T>{
+public class LinkedList<T> {
 
     Node<T> head;
     Node<T> tail;
@@ -14,12 +14,61 @@ public class LinkedList<T>{
         }
     }
 
+    public void append(T data) {
+        Node<T> newNode = new Node<>(data);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+    }
+
+    public void firstElementDelete() {
+        if (head == null) {
+            System.out.println("Linked list is Empty");
+        }
+        head = head.next;
+    }
+
+    public void lastElementDelete() {
+        if (head == null) {
+            System.out.println("Linked list is Empty");
+        } else if (head == tail) {
+            head = null;
+            tail = null;
+        } else {
+            Node<T> currentNode = head;
+            while (currentNode.next != tail) {
+                currentNode = currentNode.next;
+            }
+            currentNode.next = null;
+            tail = currentNode;
+        }
+    }
+    public Node<T> search(T data) {
+        Node<T> currentNode = head;
+        while (currentNode != null) {
+            if (currentNode.data.equals(data)) {
+                return currentNode;
+            }
+            currentNode = currentNode.next;
+        }
+        return null;
+    }
     public void display() {
-        Node<T> temp = head;
-        while (temp != null) {
-            System.out.print(temp.data + " -> ");
-            temp = temp.next;
+        //Node currentNode = head;
+        Node<T> currentNode = head;
+        if (head == null) {
+            System.out.println("Linked List is Empty");
+        }
+        while (currentNode != null) {
+            System.out.print(currentNode.data + " -> ");
+            currentNode = currentNode.next;
         }
         System.out.println();
     }
-}
+    }
+
+
